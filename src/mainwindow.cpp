0e14352fa78eeba958493e0b947a860d650c7c96
Tomas Sedivy <sedivyt8@gmail.com>
2016-04-12 09:35:15 +0200

    File dialog for opening images and automatic rescaling

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -5,6 +5,9 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
+#include <QDir>
+#include <QFileDialog>
+
 using namespace cv;
 
 cv::Mat img, gray;
@@ -46,7 +49,7 @@ void CannyThreshold(int, void*)
 	dst = Scalar::all(0);
 
 	gray.copyTo( dst, detected_edges);
-	imshow( window_name, dst );
+    imshow( window_name, dst );
 }
 
 void MainWindow::on_actionOpenCVCanny_triggered()
@@ -63,7 +66,7 @@ void MainWindow::on_actionOpenCVCanny_triggered()
 	namedWindow( window_name, WINDOW_AUTOSIZE );// Create a window for display.
 
 	/// Create a Trackbar for user to enter threshold
-	createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
+    createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
 
 	/// Show the image
 	CannyThreshold(0, 0);
@@ -74,7 +77,30 @@ void MainWindow::on_actionOpenCVCanny_triggered()
 
 void MainWindow::on_action_Load_triggered()
 {
-	img = imread("../lena.jpg", CV_LOAD_IMAGE_COLOR);
+    QString fileName = QFileDialog::getOpenFileName(this,
+        tr("Open Image"), QDir::currentPath(), tr("Image Files (*.png *.jpg *.bmp)"));
+
+//    QString fileName = QFileDialog::getOpenFileName(this,         //to je moje
+//        tr("Open Image"), "/usr/share/backgrounds/linuxmint-rosa", tr("Image Files (*.png *.jpg *.bmp)"));
+
+    img = imread(fileName.toStdString(), CV_LOAD_IMAGE_COLOR);
+
+    if (img.rows > 512 || img.cols > 512 ) { // zavest konstantu
+        double scale;
+        double height = static_cast<double>(img.rows);
+        double width = static_cast<double>(img.cols);
+        if (img.rows > img.cols) {
+            scale = height / 512.0;
+        } else {
+            scale = width / 512.0;
+        }
+
+        cv::Size size((int) round(width / scale), (int) round(height / scale));
+        cv::Mat src = img;
+        cv::resize(src, img, size);
+    }
+
+
 	QPixmap p = QPixmap::fromImage(QImage((unsigned char*) img.data, img.cols, img.rows, QImage::Format_RGB888).rgbSwapped());
 	ui->label->setPixmap(p);
 	ui->label->setFixedSize(p.size());
